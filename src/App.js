3a26327f2ec821cb8e3a1d8be4360d6524437bf8
Maurice Jones <jones.maurice39@yahoo.com>
2021-06-05 22:19:56 -0700

    fade done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-// eslint-disable-next-line
 import Header from './components/Header'
 import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />  */}
+      <Header /> 
       <Home />
     </div>
   );
